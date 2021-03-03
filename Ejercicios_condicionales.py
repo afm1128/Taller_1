8a7302fb5bb75c4abd3ecdb818e364b7d00dab0f
Andres Mendoza <afma102831@gmail.com>
2021-03-03 10:51:52 -0500

    final taller condicional

--- Ejercicios_condicionales.py
+++ Ejercicios_condicionales.py
@@ -98,3 +98,118 @@ else:
     print(f'la persona debe comprar el carro')
 
 
+# En una fábrica de computadoras se planea ofrecer a los clientes un
+# descuento que dependerá del número de computadoreas que
+# compre. Si las computadoras son menos de cinco se les dará un 10%
+# de descuento sobre el total de la compra; si el número de
+# computadoras es mayor o igual a cinco pero menos de diez se le
+# otorga un 20% de descuento; y si son 10 o más se les da un 40%. El
+# precio de cada computadora es de $11.000.
+
+
+p_computadora = 11000
+cantidad = int(input("Ingrese la cantidad de computadoras a comprar: "))
+
+v_total = p_computadora*cantidad
+
+if(cantidad < 5):
+    v_pagar1 = v_total * 0.9
+    print(f'El precio a pagar es: ${v_pagar1: ,}')
+     
+elif(cantidad >= 5 and cantidad < 10):
+    v_pagar2 = v_total * 0.8
+    print(f'El precio a pagar es: ${v_pagar2: ,}')
+     
+else:
+    v_pagar3 = v_total * 0.6
+    print(f'El precio a pagar es: ${v_pagar3: ,}')
+
+# Un proveedor de estéreos ofrece un descuento del 10% sobre el
+# precio sin IVA, de algún aparato si este cuesta $2000 o más. Además,
+# independientemente de esto, ofrece un 5% de descuento si la marca
+# es NOSY. Determinar cuanto pagará, con IVA incluido, un cliente
+# cualquiera por la compra de su aparato. IVA es del 16%.
+
+marca = input('Ingrese marca del articulo: ')
+compra = int(input('Valor estereos: $'))
+
+if(compra > 2000 and compra and marca == 'NOSY' or marca == 'nosy'):
+    
+    v_estereo = compra * 0.85 
+    e_iva = v_estereo + (v_estereo * 0.16)
+    print(f'El ciente paga: ${e_iva: ,}')
+
+elif(compra > 2000):
+     v_estereo2 = compra * 0.90 
+     e_iva2 = v_estereo2 + (v_estereo2 * 0.16)
+     print(f'El ciente paga: ${e_iva2: ,}') 
+
+else:
+    e_iva3 = compra + (compra * 0.16)
+    print(f'El ciente paga: ${e_iva3: ,}') 
+
+# Una empresa quiere hacer una compra de varias piezas de la misma
+# clase a una fábrica de refacciones. La empresa, dependiendo del
+# monto total de la compra, decidirá que hacer para pagar al fabricante.
+# Si el monto total de la compra excede de $500.000 la empresa tendrá
+# la capacidad de invertir de su propio dinero un 55% del monto de la
+# compra, pedir prestado al banco un 30% y el resto lo pagará
+# solicitando un crédito al fabricante. Si el monto total de la compra no
+# excede de $500.00 la empresa tendrá capacidad de invertir de su
+# propio dinero un 70% y el restante 30% lo pagará solicitando crédito
+# al fabricante. El fabricante cobra por concepto de interes un 20%
+# sobre la cantidad que se le pague a crédito. Obtener la cantidad a
+# inverir, valor del préstamo, valor del crédito y los intereses.
+
+n_piezas = int(input ('digite el numero de piezas que desea comprar: '))
+v_pieza = int(input('diga el valor de unitario de las piezas: $'))
+v_pagar = n_piezas * v_pieza
+if(v_pagar > 500000):
+    p_empresa = v_pagar * 0.55
+    p_banco = v_pagar * 0.30
+    c_fabricante = v_pagar * 0.15
+    c_intereses = c_fabricante * 0.20
+    print(f'el cliente pagara de su propio dinero : ${p_empresa: ,}')
+    print(f'el cliente con prestamo del banco : ${p_banco: ,}')
+    print(f'el cliente pagara con credito : ${c_fabricante: ,}')
+    print(f'el cliente pagara por intereses del credito : ${c_intereses: ,}')
+
+else:
+    p_empresa1 = v_pagar * 0.70
+    c_fabricante1 = v_pagar * 0.30
+    c_intereses1 = c_fabricante1 * 0.20
+    print(f'el cliente pagara de su propio dinero : ${p_empresa1: ,}')
+    print(f'el cliente pagara con credito : ${c_fabricante1: ,}')
+    print(f'el cliente pagara por intereses del credito : ${c_intereses1: ,}')
+
+# Leer 2 números; si son iguales que lo multiplique, si el primero es
+# mayor que el segundo que los reste y sino que los sume.
+
+numero_1 = int(input('diga el primer numero: '))
+numero_2 = int(input('diga el segundo numero: '))
+if (numero_1 == numero_2):
+    mult = numero_1 * numero_2
+    print(f' como son iguales se multiplica y el resultado es: {mult}')
+
+elif(numero_1 > numero_2):
+    resta = numero_1 - numero_2
+    print(f' numero 1 mayor que numero 2 se restan y el resultado es: {resta}')
+
+else:
+    suma = numero_1 + numero_2
+    print(f' numero 1 menor que numero 2 se suman y el resultado es: {suma}')
+
+# Leer tres números diferentes e imprimir el número mayor de los
+# tres.
+
+numero_1 = int(input('diga el primer numero: '))
+numero_2 = int(input('diga el segundo numero: '))
+numero_3 = int(input('diga el tercer numero: '))
+if(numero_1 > numero_2 and numero_1 > numero_3):
+    print(f' el mayor de los numeros es: {numero_1}')
+
+elif(numero_2 > numero_1 and numero_2 > numero_3):
+    print(f' el mayor de los numeros es: {numero_2}')
+
+else:
+    print(f' el mayor de los numeros es: {numero_3}')
